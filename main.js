d395eaaf119d248727b22a3808984a76620c6ede
lahdesign <lahdesign@gmail.com>
2018-10-24 11:21:27 -0400

    changes to starter code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('got here')
 
 const onMoonClick = function () {
+   console.log('got into function')
     $('#message').text('Welcome to the moon.')
     $('#message').css('background-Color', 'blue')
     setTimeout(() => $('#message').text(''), 3000)
