a73447031a99d0c8efd2811c29eac56171c63cbf
lahdesign <lahdesign@gmail.com>
2019-03-19 21:17:12 -0400

    updated styling, changed background image

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const onMoonClick = function () {
     $('#message').text('Welcome to the moon.')
     $('#message').css('background-Color', 'blue')
     setTimeout(() => $('#message').text(''), 3000)
-    $('#welcomeContainer').hide()
-    $('#firstPage').show()
+    $('.container').addClass('animated rollOut');
+    $('#welcomePage').addClass('animated rollIn')
   }
 
 const addHandlers = () => {
