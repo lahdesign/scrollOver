5943d245c8ebcdf96f1adff92ccc89b568bc88f6
lahdesign <lahdesign@gmail.com>
2019-03-26 09:03:25 -0400

    made style changes to button

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 // console.log('got here')
 
-const onMoonClick = function () {
+const onButtonClick = function () {
   //  console.log('got into function')
     $('#message').text('Climate change is affecting our community.')
     $('#message').css('background-Color', 'purple')
     setTimeout(() => $('#message').text('Learn ways that you can help'), 3000)
+    $('#nextView').text('You have made a great choice.');
     $('.container').addClass('animated rollOut');
+    $('.container').hide();
     $('#welcomePage').addClass('animated rollIn')
   }
 
 const addHandlers = () => {
-    $('#nextView').on('click', onMoonClick)
+    $('#nextView').on('click', onButtonClick)
   }
 
   $(() => {
